ddbde5d69c30377133fc735508c0d71e3a55acbb
carlos gizbert <carlosgizbert@gmail.com>
2022-01-17 10:54:49 -0300

    add validação

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,11 +4,17 @@ import api from '../../api'
 import { Formik, Form, Field } from 'formik'
 import { useNavigate } from 'react-router'
 import {ReactComponent as Logo} from '../../assets/logo.svg'
+import * as Yup from 'yup'
 
 const initialState = {
   email: '',
   password: ''
 }
+
+const schema = Yup.object().shape({
+  email: Yup.string().required("Insira seu E-mail"),
+  password: Yup.string().required("Insira sua Senha")
+})
 
 const Login = () => { 
   const [values, setValues] = useState(initialState)
@@ -20,8 +26,9 @@ const Login = () => {
   }
 
   // precisa usar navigate como hook dentro do componente função
-  const handleLogin = e => {
-    api.post('/users/login', values)
+  const handleLogin = async e => {
+    e.preventDefault();
+    await api.post('/users/login', values)
     .then(res => 
       {
         const { data } = res
@@ -31,33 +38,46 @@ const Login = () => {
           console.log(data)
         }
       }
-    )
+    ).catch(e => console.log(e))
   }
+
 
   return(
     <div className="login-page">
       <div className="login-box">
       <div className="logo"><Logo/></div>
-      <Formik initialValues={{}} onSubmit={handleLogin}>
-        <Form className='fields mt-20'>
+      <Formik 
+      initialValues={{}} 
+      validationSchema={schema}>
+        {({ errors }) => (
+          <Form className='fields mt-20' 
+          onSubmit={handleLogin}>
+            {(errors.email || errors.password) &&
+            <div className="form-erros">
+              <ul>
+              {errors.email && <li>{errors.email}</li>}
+              {errors.password && <li>{errors.password}</li>}
+              </ul>
+            </div>}
           <span htmlFor="email">E-mail</span>
-          <Field 
-          className="field" 
-          type="text"
-          name="email" 
-          placeholder="Insira seu email" 
-          onChange={onChange}
-          />
+            <Field 
+            className="field" 
+            type="text"
+            name="email" 
+            placeholder="Insira seu email" 
+            onChange={onChange}
+            />
           <span htmlFor="password">Senha</span>
-          <Field 
-          className="field" 
-          type="password"
-          name="password" 
-          placeholder="Insira sua senha" 
-          onChange={onChange}
-          />
-          <input className="bt-login" type="submit" value="Entrar"></input>
+            <Field 
+            className="field" 
+            type="password"
+            name="password" 
+            placeholder="Insira sua senha" 
+            onChange={onChange}
+            />
+          <input className="bt bt-primary" type="submit" value="Entrar"></input>
         </Form>
+        )}
       </Formik>
       </div>
     </div>
