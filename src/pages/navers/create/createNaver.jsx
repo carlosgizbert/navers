8c655203d686f7c190da14e23f8137ca85699229
carlos gizbert <carlosgizbert@gmail.com>
2022-01-17 13:31:04 -0300

    ajuste voltar

--- src/pages/navers/create/createNaver.jsx
+++ src/pages/navers/create/createNaver.jsx
@@ -74,7 +74,7 @@ const handleCloseModal = (e) => {
       <div className="wrapper">
         <div className="create-header">
           <Link to="/navers">
-          <div className="bt-voltar"><BtVoltar/></div>
+          <div className="bt-icon bt-voltar"><BtVoltar/></div>
           </Link>
           <h1>Adicionar Naver</h1>
         </div>
